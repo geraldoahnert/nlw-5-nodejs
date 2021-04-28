7765e2baf4b1d94bea3734205aa9f0a462a82afb
geraldoam <geraldoahnert@gmail.com>
2021-04-28 15:46:15 -0300

    Update: Using express with GET and POST http request.

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,28 @@ import express from "express";
 
 const app = express();
 
+/* 
+
+* Methods - HTTP
+
+-> GET = Search
+-> POST = Create
+-> PUT = Alter
+-> DELETE = Delete
+-> PATCH = Alter an specific information.
+
+*/
+
+app.get("/", (request, response) => {
+    return response.json({
+        message: "Olá, NLW 05."
+    })
+})
+
+app.post("/", (request, response) => {
+    return response.json({
+        message: "Usuário salvo com sucesso!"
+    })
+})
+
 app.listen(3333, () => console.log('Server is running on port 3333.'));
