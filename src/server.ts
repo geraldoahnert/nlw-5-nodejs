6e235c578d923f8fbe4e8a375413cc83ba646184
geraldoam <geraldoahnert@gmail.com>
2021-04-30 20:42:26 -0300

    Update: Using routes.

--- src/server.ts
+++ src/server.ts
@@ -1,30 +1,11 @@
 import express from "express";
 import "./database"; // index.ts
+import { routes } from "./routes";
 
 const app = express();
 
-/* 
+app.use(express.json());
 
-Methods - HTTP
-
--> GET = Search
--> POST = Create
--> PUT = Alter
--> DELETE = Delete
--> PATCH = Alter an specific information.
-
-*/
-
-app.get("/", (request, response) => {
-    return response.json({
-        message: "Olá, NLW 05."
-    })
-})
-
-app.post("/", (request, response) => {
-    return response.json({
-        message: "Usuário salvo com sucesso!"
-    })
-})
+app.use(routes);
 
 app.listen(3333, () => console.log('Server is running on port 3333.'));
