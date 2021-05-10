0563f4ab6a13a99219f98277b62d1f75d48230f7
geraldoam <geraldoahnert@gmail.com>
2021-05-10 19:04:25 -0300

    Fix: Create findByEmail method.

--- src/services/UsersService.ts
+++ src/services/UsersService.ts
@@ -4,7 +4,6 @@ import { UsersRepository } from "../repositories/UsersRepository";
 
 class UsersService {
     private usersRepository: Repository<User>;
-    findByEmail: any;
     constructor() {
         this.usersRepository = getCustomRepository(UsersRepository);
     }
@@ -24,6 +23,12 @@ class UsersService {
 
         await this.usersRepository.save(user);
 
+        return user;
+    }
+    async findByEmail(email: string) {
+        const user = await this.usersRepository.findOne({
+            email,
+        });
         return user;
     }
 }
